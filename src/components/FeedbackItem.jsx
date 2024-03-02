eaefacabf6ec985a37eb46a33d090dac241e9a6a
Erick <12631133+erick207@users.noreply.github.com>
2024-03-02 13:03:55 -0300

    use Global State to Display List

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,16 +1,8 @@
-// all hooks start by 'use'
-import { useState } from "react"
-
-function FeedbackItem() {
-  // to change a piece of state we have to call one of the `sets`
-  // state in React is inmutable, meaning it has to be reset to be changed
-  const [rating, setRating] = useState(7)
-  const [text, setText] = useState('This is an example of a feedback item')
-
+function FeedbackItem({item}) {
   return (
     <div className='card'>
-        <div className="num-display">{rating}</div>
-        <div className="text-display">{text}</div>
+        <div className="num-display">{item.rating}</div>
+        <div className="text-display">{item.text}</div>
     </div>
   )
 }
