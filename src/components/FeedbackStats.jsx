6c3075da370a70ea1cab2f2700006bc1af14e11c
Erick <12631133+erick207@users.noreply.github.com>
2024-03-04 14:43:51 -0300

    delete unneeded log

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -9,8 +9,6 @@ function FeedbackStats({feedback}) {
   
   average = average.toFixed(1).replace(/[.,]0$/, '')
 
-  console.log(average)
-
   return <div className="feedback-stats">
     <h4>{feedback.length} Reviews</h4>
     <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
