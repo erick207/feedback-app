8d5645fe9eafdb6be1d77bcf9a1e8d60b08e21a7
Erick <12631133+erick207@users.noreply.github.com>
2024-03-05 10:58:00 -0300

    framer motion animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,21 +1,45 @@
+import {motion, AnimatePresence} from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
 function FeedbackList({feedback, handleDelete}) {
   if(!feedback || feedback.length === 0) return <p>No feedback yet</p>
   
+
   return (
-    <div className="feedback-list">
-      {feedback.map((item) => ( 
-        <FeedbackItem 
-          key = {item.id} 
-          item = {item} 
-          handleDelete = {handleDelete} 
-        />
-      ))}
-    </div>
+  <div className="feedback-list">
+    {feedback.map((item) => ( 
+      <FeedbackItem 
+        key = {item.id} 
+        item = {item} 
+        handleDelete = {handleDelete} 
+      />
+    ))}
+  </div>
   )
 }
+
+// TODO: fix animation
+/*  return (
+    <div className="feedback-list">
+      <AnimatePresence>
+      {feedback.map((item) => ( 
+        <motion.div
+          key={item.id}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity:0}}
+        >
+          <FeedbackItem 
+            key = {item.id} 
+            item = {item} 
+            handleDelete = {handleDelete} 
+          />
+        </motion.div>
+      ))}
+      </AnimatePresence>
+    </div>
+  ) */
 
 FeedbackList.propTypes = {
   // we could simple do:
