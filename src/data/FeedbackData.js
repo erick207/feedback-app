3d64adc609538a0fed2a9b68f96b644647cbfe2e
Erick <12631133+erick207@users.noreply.github.com>
2024-03-04 23:46:05 -0300

    make IDs UUIDv4 format

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,16 +1,16 @@
 const FeedbackData = [
   {
-    id: 1,
+    id: "8ae89853-d0b1-4477-a5a6-cc8a100579a2",
     rating: 10,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
   {
-    id: 2,
+    id: "8ae89853-d0b1-4477-a5a6-cc8a100579a3",
     rating: 9,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
   {
-    id: 3,
+    id: "8ae89853-d0b1-4477-a5a6-cc8a100579a4",
     rating: 8,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
