099277dbb58cb36d59e1875cd1a0b80268824271
Erick <12631133+erick207@users.noreply.github.com>
2024-03-07 10:22:20 -0300

    edit feedback event

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -25,21 +25,37 @@ export const FeedbackProvider = ({ children }) => {
     },
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
+
+  // add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback]) // state is immutable, so we need to copy it, the new feedback is put on front
   }
 
+  // delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
+  // set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
+  }
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        editFeedback,
         deleteFeedback,
         addFeedback,
       }}
