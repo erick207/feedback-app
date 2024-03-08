091b9ba463a474ac111eb8afd356356c61f25cbc
Erick <12631133+erick207@users.noreply.github.com>
2024-03-08 19:10:49 -0300

    create mock backend, fetch data and spinner state

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,34 +1,32 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true)
   // there are other ways of doing the state besides using useState
   // like using reducers but since this is a small app let's keep it simple
 
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This is feedback item 2',
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: 'This is feedback item 3',
-      rating: 7,
-    },
-  ])
+  const [feedback, setFeedback] = useState([])
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`http://localhost:5000/feedback`)
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   // add feedback
   const addFeedback = (newFeedback) => {
@@ -63,6 +61,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit, // actual piece of state that holds the bool for the form
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback, // this is the function handler
